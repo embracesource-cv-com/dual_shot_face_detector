17ccf9a147f1f8bc4a342dbd4a4c19de187818ce
danna.li <danna.li@embracesource.com>
2019-06-03 17:25:36 +0800

    small edit

--- dual_conf.py
+++ dual_conf.py
@@ -10,18 +10,20 @@ import numpy as np
 
 class Config(object):
     data_path = '/home/dataset/face_recognize/face_detect'
-    output_dir = '/home/dataset/face_recognize/face_detect'
+    output_dir = '/opt/code/face/dual_shot/train_out'
     num_class = 2
     net_in_size = 640
     num_anchor = 34125
     num_image = 12876
     batch_size = 4
-    epochs = 10000
-    steps_per_epoch = 30 # 约3000/epoch
+    epochs = 100
+    steps_per_epoch = 3000  # 约3000/epoch
     gpu_index = "1"
     num_train_anchor = 20
     iou_thread = 0.4
+
     lr = 0.001
+    weight_decay = 0.00001
 
     continue_training = False
     weights_to_transfer = '/opt/pretrained_model/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
