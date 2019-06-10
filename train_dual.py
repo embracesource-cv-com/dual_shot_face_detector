eb83d3f7309b890de24645222f770d756eb8f8ce
danna.li <danna.li@embracesource.com>
2019-06-10 10:42:03 +0800

    add learning rate schedule

--- train_dual.py
+++ train_dual.py
@@ -11,14 +11,27 @@ from dual_conf import current_config as conf
 from net.dual_shot import train_net
 import keras
 from keras import Model, Input
-from keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+from keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint, LearningRateScheduler
 from keras.optimizers import SGD
 import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = conf.gpu_index
 
+
+def lr_scheduler(epoch):
+    if epoch > 8.3 * conf.epochs:
+        lr = 0.00001
+    elif epoch > 0.67 * conf.epochs:
+        lr = 0.0001
+    else:
+        lr = 0.001
+    print('lr: %f' % lr)
+    return lr
+
+
 log = TensorBoard(log_dir=conf.output_dir, )
-lr_decay = ReduceLROnPlateau(monitor='loss', patience=2, factor=0.95)
+lr_decay = LearningRateScheduler(lr_scheduler)
+# lr_decay = ReduceLROnPlateau(monitor='loss', patience=2, factor=0.95)
 ckpt_saver = ModelCheckpoint(monitor='loss', filepath=os.path.join(conf.output_dir, 'weights.h5'), verbose=1,
                              save_best_only=True, save_weights_only=True)
 callback = [log, lr_decay, ckpt_saver]
@@ -44,7 +57,7 @@ if conf.continue_training:
     print('loading trained weights from..', conf.weights_to_transfer)
     model.load_weights(conf.weights_to_transfer, by_name=True)
 
-model.compile(optimizer=SGD(lr=conf.lr, decay=conf.weight_decay,momentum=0.9), loss=None)
+model.compile(optimizer=SGD(lr=conf.lr, decay=conf.weight_decay, momentum=0.9), loss=None)
 model.fit_generator(generator=gen,
                     validation_steps=2,
                     steps_per_epoch=conf.steps_per_epoch,
